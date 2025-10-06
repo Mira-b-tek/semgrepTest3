35250b7a9bd5a84280973ccb3ca7316e201df795
Mira Abu Rahmeh <mira.a@b-tek.dev>
2025-10-06 23:35:43 +0300

    first commit

---
+++ src/main/java/com/example/testSemgrep/TestSemgrepApplication.java
@@ -0,0 +1,14 @@
+package com.example.testSemgrep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSemgrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSemgrepApplication.class, args);
+		System.out.println("test semgrep");
+	}
+
+}
---
+++ src/test/java/com/example/testSemgrep/TestSemgrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testSemgrep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSemgrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
